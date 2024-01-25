568a6fdbb460de69e76b854b39ce243039657e91
maxo <maxolotikashvili@yahoo.com>
2024-01-25 17:13:48 +0400

    update header-complete

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let isBurgerResponsive = false;
 
 function openResponsiveBurger() {
     const burger = document.getElementById('burger');
-    
+    const nav = document.getElementById('navigation');
+
     if (!isBurgerResponsive) {
         burger.classList.remove('burger-wrapper');
         burger.classList.add('burger-wrapper-responsive');
+        nav.classList.add('nav-responsive')
         isBurgerResponsive = true;
     } else {
         burger.classList.remove('burger-wrapper-responsive');
         burger.classList.add('burger-wrapper');
+        nav.classList.remove('nav-responsive');
         isBurgerResponsive = false;
     }
 }
